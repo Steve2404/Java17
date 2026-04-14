e51bdc46d5491d11cb22c06d5398769719c8f986
Steve2404 <81958298+Steve2404@users.noreply.github.com>
2026-04-14 05:23:03 +0200

    begin of build method buildTicketAndSearch

--- src/main/java/com/string/Invoice.java
+++ src/main/java/com/string/Invoice.java
@@ -6,32 +6,25 @@ public class Invoice {
         int[] qty = {3, 12, 1};
         double [] unit = {0.59, 1.25, 1.09};
 
-        String rowFormat = "|%-15s |%5s |%10s |%10s|%n";
-        String dataFormat = "|%-15s |%5d |%10.2f |%10.2f|%n";
+        System.out.printf("*%-15s *%5s *%10s *%10s*%n", "-".repeat(15), "-".repeat(5), "-".repeat(10), "-".repeat(10));
+        System.out.printf("|%-15s |%5s |%10s |%10s|%n", "ITEM", "QTY", "UNIT", "TOTAL");
+
+        System.out.printf("*%-15s *%5s *%10s *%10s*%n", "-".repeat(15), "-".repeat(5), "-".repeat(10), "-".repeat(10));
 
         double total = 0.0;
-
-        printBorder();
-        System.out.printf(rowFormat, "ITEM", "QTY", "UNIT", "TOTAL");
-        printBorder();
-
+        double cost = 0.0;
         for (int i = 0; i < items.length; i++) {
-            double cost = qty[i] * unit[i];
+            cost = qty[i] * unit[i];
             total += cost;
 
 
-             System.out.printf(dataFormat, items[i], qty[i], unit[i], cost);
+             System.out.printf("|%-15s |%5d |%10.2f |%10.2f|%n", "ITEM", qty[i], unit[i], cost);
 
         }
 
-        System.out.printf(rowFormat, " ", " ", " ", " ");
-        printBorder();
+        System.out.printf("|%-15s |%5s |%10s |%10s|%n", " ", " ", " ", " ");
+        System.out.printf("*%-15s *%5s *%10s *%10s*%n", "-".repeat(15), "-".repeat(5), "-".repeat(10), "-".repeat(10));
         System.out.printf("|%-15s |%5s %10s %12.2f|%n", "Total", " ", " ", total);
-        printBorder();
-    }
-
-    public static void printBorder() {
-        System.out.printf("*%-15s *%5s *%10s *%10s*%n",
-                "-".repeat(15), "-".repeat(5), "-".repeat(10), "-".repeat(10));
+        System.out.printf("*%-15s *%5s *%10s *%10s*%n", "-".repeat(15), "-".repeat(5), "-".repeat(10), "-".repeat(10));
     }
 }
